335e0b422b54dbbaaf34fe1a3b72e5c978a78496
shubham <shubhamganeshsonawane11@gmail.com>
2022-09-16 17:25:12 +0530

    #Day5

---
+++ 5_Greatest_of_two_numbers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num1 = 23;
+    int num2 = 43;
+
+    if(num1>num2){
+        cout<<num1;
+    }
+    else{
+        cout<<num2;
+    }
+    return 0;
+}
+// Time Complexity : O(1)
+// Space Complexity : O(1)
